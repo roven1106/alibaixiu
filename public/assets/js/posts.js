8e228b749b9b2cb158b85380925036dbd4e2a9ea
luoshuwen <1521616767@qq.com>
2019-10-17 20:35:42 +0800

    完成更改

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -1,13 +1,40 @@
-$.ajax({
-    type:'get',
-    url:'/posts',
-    success:function(res){
-        var html = template('postsTpl',res);
-        $('#postsBox').html(html);
-    }
-});
 
-function dateFormat(date){
-    date = new Date(date);
-    return date.getFullYear()+'年'+(date.getMonth()+1)+'月'+date.getDate()+'日';
+  $.ajax({
+    type: "get",
+    url: "/posts",
+    success: function(res) {
+      var html = template("postsTpl", res);
+      $("#postsBox").html(html);
+      var page = template("pageTpl", res);
+      $("#page").html(page);
+    }
+  });
+
+
+function dateFormat(date) {
+  date = new Date(date);
+  return (
+    date.getFullYear() +
+    "年" +
+    (date.getMonth() + 1) +
+    "月" +
+    date.getDate() +
+    "日"
+  );
+}
+
+function changePage(page){
+    $.ajax({
+        type:'get',
+        url:'/posts',
+        data:{
+            page
+        },
+        success:function(res){
+            var html = template('postsTpl',res);
+            $('#postsBox').html(html);
+            var page = template("pageTpl", res);
+            $("#page").html(page);
+        }
+    });
 }
